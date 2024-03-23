b555b7df3958407e1bf36c5d7757dc41a816a88c
Ahsan-Tariq-565 <ahsanpimsat@gmail.com>
2024-03-22 21:01:47 -0700

    last comment

---
+++ 11. Greeting/12_greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Ahsan', 'Adeel', 'Faraz', 'Ubaid', 'Aamir', 'Hamza', 'Ansar'];
+const messsage = 'Welcome to Typescript Friends: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(messsage + names[i]);
+}
